e5ac767f06b60b8bdf363e1dd9f6c6be128e06ce
Compunova <>
2017-01-25 19:59:35 +0300

    Linode provider improvements

--- kozinaki/providers/libcloud_driver/extended_drivers.py
+++ kozinaki/providers/libcloud_driver/extended_drivers.py
@@ -1,11 +1,13 @@
 from libcloud.utils.py3 import httplib
 from libcloud.compute.types import Provider
 from libcloud.compute.providers import get_driver
+from libcloud.common.linode import API_ROOT
 
 
 def get_extended_driver(driver_cls):
     extended_drivers = {
-        'Vultr': VultrNodeDriverExt
+        'Vultr': VultrNodeDriverExt,
+        'Linode': LinodeNodeDriverExt,
     }
 
     return extended_drivers[driver_cls.name] if driver_cls.name in extended_drivers.keys() else driver_cls
@@ -24,3 +26,16 @@ class VultrNodeDriverExt(get_driver(Provider.VULTR)):
         res = self.connection.post('/v1/server/start', params)
 
         return res.status == httplib.OK
+
+
+class LinodeNodeDriverExt(get_driver(Provider.LINODE)):
+
+    def ex_shutdown_node(self, node):
+        params = {"api_action": "linode.shutdown", "LinodeID": node.id}
+        self.connection.request(API_ROOT, params=params)
+        return True
+
+    def ex_power_on_node(self, node):
+        params = {"api_action": "linode.boot", "LinodeID": node.id}
+        self.connection.request(API_ROOT, params=params)
+        return True
