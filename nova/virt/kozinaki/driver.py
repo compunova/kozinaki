945e30a15bf7ee3ca9f4295ab6b659b83760c075
Compunova <>
2017-02-09 22:46:30 +0300

    Merge fix

--- nova/virt/kozinaki/driver.py
+++ nova/virt/kozinaki/driver.py
@@ -40,16 +40,13 @@ _LOG = "\n\n### {} ###\n\n"
 
 
 CONF = cfg.CONF
-<<<<<<< Updated upstream:kozinaki/driver.py
-CONF.import_opt('my_ip', 'nova.netconf')
-=======
 
 # In OpenStack Newton netconf moved to nova.conf
 try:
     CONF.import_opt('my_ip', 'nova.netconf')  # Metaka
 except ImportError as e:
     CONF.import_opt('my_ip', 'nova.conf.netconf')  # Newton
->>>>>>> Stashed changes:nova/virt/kozinaki/driver.py
+
 
 # Mapping of libcloud instance power states to the OpenStack power states
 # Each libcloud driver has provider specific power states in NODE_STATE_MAP within each driver
